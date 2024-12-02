df773d89f2985b392caab25d806ee9326641e109
TagadG <famille.aribaud6@gmail.com>
2024-12-02 10:02:04 +0100

    Add files via upload

---
+++ Osint.js
@@ -0,0 +1,23 @@
+function verifierCode() {
+    var codeCorrect = "FREAKBOB";
+    var codeSaisi = document.getElementById("codeInput").value;
+    
+    var messageDiv = document.getElementById("message1");
+
+    if (codeSaisi === codeCorrect) {
+        messageDiv.textContent = "Votre coupon est: asqjqlqwaqvb@ouiqt.kwu";
+        messageDiv.style.color = "green";
+        messageDiv.style.display = "block";
+    } else {
+        messageDiv.textContent = "Code incorrect, essayez à nouveau.";
+        messageDiv.style.color = "red";
+        messageDiv.style.display = "block";
+    }
+}
+
+function AfficherCle() {
+	var messageDiv = document.getElementById("message2");
+	messageDiv.textContent = "Clé du cryptage César:8";
+    messageDiv.style.color = "white";
+    messageDiv.style.display = "block";
+}
